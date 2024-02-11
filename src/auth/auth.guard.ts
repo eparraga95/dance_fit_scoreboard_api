e2ce851a485ddf10ce15b25d245ac6fffe19bd7b
Eduardo Parraga <eduardo.parraga95@gmail.com>
2024-02-11 14:33:03 -0300

    fix: auth guard logic to read user role

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -19,35 +19,38 @@ export class AuthGuard implements CanActivate {
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
-    const requestToken: string = request.headers.authorization.split(' ')[1];
+    try {
+      const request = context.switchToHttp().getRequest();
+      const requestToken: string = request.headers.authorization.split(' ')[1];
 
-    if (!requestToken) {
-      throw new UnauthorizedException('No Token Found');
+      if (!requestToken) {
+        throw new UnauthorizedException('No Token Found');
+      }
+
+      const session = await this.sessionRepository.findOne({
+        where: { token: requestToken },
+      });
+
+      if (!session) {
+        throw new UnauthorizedException('Invalid token');
+      }
+
+      const now = new Date();
+
+      const decodedJwt = this.jwtService.decode(session.token);
+
+      const jwtExpiration = new Date(decodedJwt.exp * 1000);
+
+      if (now > jwtExpiration) {
+        throw new UnauthorizedException('Session expired');
+      }
+
+      request.user = this.jwtService.verify(session.token, {
+        secret: process.env.jwtSecret,
+      });
+      return true;
+    } catch (err) {
+      console.log(err);
     }
-
-    const session = await this.sessionRepository.findOne({
-      where: { token: requestToken },
-    });
-
-    if (!session) {
-      throw new UnauthorizedException('Invalid token');
-    }
-
-    const now = new Date();
-
-    const decodedJwt = this.jwtService.decode(session.token);
-
-    const jwtExpiration = new Date(decodedJwt.exp * 1000);
-
-    if (now > jwtExpiration) {
-      throw new UnauthorizedException('Session expired');
-    }
-
-    request.user = this.jwtService.verify(session.token, {
-      secret: process.env.jwtSecret,
-    });
-
-    return true;
   }
 }
