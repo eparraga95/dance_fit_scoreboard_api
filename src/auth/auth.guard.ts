bec1a3a1ada721ad6de7bea1dae7b0e2165a4cd6
Eduardo Parraga <eduardo.parraga95@gmail.com>
2024-02-11 16:49:40 -0300

    removed try/catch logic

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -19,38 +19,35 @@ export class AuthGuard implements CanActivate {
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    try {
-      const request = context.switchToHttp().getRequest();
-      const requestToken: string = request.headers.authorization.split(' ')[1];
+    const request = context.switchToHttp().getRequest();
+    const requestToken: string = request.headers.authorization.split(' ')[1];
 
-      if (!requestToken) {
-        throw new UnauthorizedException('No Token Found');
-      }
-
-      const session = await this.sessionRepository.findOne({
-        where: { token: requestToken },
-      });
-
-      if (!session) {
-        throw new UnauthorizedException('Invalid token');
-      }
-
-      const now = new Date();
-
-      const decodedJwt = this.jwtService.decode(session.token);
-
-      const jwtExpiration = new Date(decodedJwt.exp * 1000);
-
-      if (now > jwtExpiration) {
-        throw new UnauthorizedException('Session expired');
-      }
-
-      request.user = this.jwtService.verify(session.token, {
-        secret: process.env.jwtSecret,
-      });
-      return true;
-    } catch (err) {
-      console.log(err);
+    if (!requestToken) {
+      throw new UnauthorizedException('No Token Found');
     }
+
+    const session = await this.sessionRepository.findOne({
+      where: { token: requestToken },
+    });
+
+    if (!session) {
+      throw new UnauthorizedException('Invalid token');
+    }
+
+    const now = new Date();
+
+    const decodedJwt = this.jwtService.decode(session.token);
+
+    const jwtExpiration = new Date(decodedJwt.exp * 1000);
+
+    if (now > jwtExpiration) {
+      throw new UnauthorizedException('Session expired');
+    }
+
+    request.user = this.jwtService.verify(session.token, {
+      secret: process.env.jwtSecret,
+    });
+    
+    return true;
   }
 }
